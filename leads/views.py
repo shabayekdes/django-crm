88a35b462bb117feec1e12f5ac7e9397160245ed
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-06-27 23:27:50 +0200

    Enhance generic import

--- leads/views.py
+++ leads/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views import generic
 
 from leads.forms import LeadForm
 from leads.models import Lead
 
 
 # Create your views here.
-class LandPageTemplateView(TemplateView):
+class LandPageTemplateView(generic.TemplateView):
     template_name = "landing.html"
 
 
 landing_page = LandPageTemplateView.as_view()
 
 
-class LeadListView(ListView):
+class LeadListView(generic.ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
     context_object_name = "leads"
@@ -23,7 +23,7 @@ class LeadListView(ListView):
 lead_list = LeadListView.as_view()
 
 
-class LeadDetailView(DetailView):
+class LeadDetailView(generic.DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
@@ -32,7 +32,7 @@ class LeadDetailView(DetailView):
 lead_detail = LeadDetailView.as_view()
 
 
-class LeadCreateView(CreateView):
+class LeadCreateView(generic.CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadForm
 
@@ -43,7 +43,7 @@ class LeadCreateView(CreateView):
 lead_create = LeadCreateView.as_view()
 
 
-class LeadUpdateView(UpdateView):
+class LeadUpdateView(generic.UpdateView):
     template_name = "leads/lead_update.html"
     queryset = Lead.objects.all()
     form_class = LeadForm
@@ -55,7 +55,7 @@ class LeadUpdateView(UpdateView):
 lead_update = LeadUpdateView.as_view()
 
 
-class LeadDeleteView(DeleteView):
+class LeadDeleteView(generic.DeleteView):
     template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
 
