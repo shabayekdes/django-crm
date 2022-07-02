a1c0c2f834ef84b8e09f81f46b67c1cd7fd4a8ed
Esmail Shabayek <esmail.shabayek@gmail.com>
2022-07-02 19:44:03 +0200

    Add logout page

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from leads import views
@@ -27,6 +27,7 @@ urlpatterns = [
     path('', views.landing_page),
     path('leads/',  include('leads.urls', namespace="leads")),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 if settings.DEBUG:
